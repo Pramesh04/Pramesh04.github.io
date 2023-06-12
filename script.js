d7d01c1da2ae813099ddb872401c38350f221b9b
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-06-12 17:56:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+   formOpenBtnp = document.querySelector("#form-openp"),
+  np = document.querySelector(".np"),
+  formContainern = document.querySelector(".form_container"),
+    formCloseBtnn = document.querySelector(".form_close"),
+    signupBtnn = document.querySelector("#signup"),
+    loginBtnn = document.querySelector("#login"),
+    pwShowHidenn = document.querySelectorAll(".pw_hide");
+  //🥲🥲🥲🥲\\
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+  
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
